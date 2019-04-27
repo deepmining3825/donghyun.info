7bfab23ea10d43224740a3b458abeacf877ca2a9
ekzm3825 <ekzm3825@gmail.com>
2019-04-28 03:13:56 +0900

    라우터 이동시 스크롤 동작 설정

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,9 @@ import SkillsView from '../views/SkillsView.vue';
 import ProjectView from '../views/ProjectView.vue';
 import ActivityView from '../views/ActivityView.vue';
 import NotFoundView from '../views/NotFoundView.vue';
+import CommerceDetail from '../views/CommerceDetail.vue';
+import SamsungcardDetail from '../views/SamsungcardDetail.vue';
+import InfochatterDetail from '../views/InfochatterDetail.vue';
 
 Vue.use(VueRouter);
 
@@ -32,8 +35,23 @@ export default new VueRouter({
             component: ActivityView,
         },
         {
+            path: '/project/commerce-chatbot',
+            component: CommerceDetail,
+        },
+        {
+            path: '/project/samsungcard-chatbot',
+            component: SamsungcardDetail,
+        },
+        {
+            path: '/project/infochatter-webmanager',
+            component: InfochatterDetail,
+        },
+        {
             path: '*', // * 설정 = 위에 설정된 path 이외의 url 모두
             component: NotFoundView,
         },
     ],
+    scrollBehavior (to, from, savedPosition) {
+        return { x: 0, y: 0 }
+    }
 });
